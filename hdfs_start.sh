6d847d04b82fd173b5c3c15eae22a00198ebbe8b
ram-chandra <ramachandra.choudki@gmail.com>
2018-06-24 19:21:44 +0000

    Update hdfs_start.sh

--- hdfs_start.sh
+++ hdfs_start.sh
@@ -1,1 +1,9 @@
 
+#!/bin/bash
+# Hadoop cluster start-up script
+# 
+# 1. Format the namenode (only required on 1st start!)
+# sudo -u hdfs hdfs namenode -format
+
+# 2. Start HDFS
+for x in `cd /etc/init.d ; ls hadoop-hdfs-*` ; do sudo service $x start ; done
