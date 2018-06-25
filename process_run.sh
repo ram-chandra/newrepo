e4efbc45f34572434547eda1c0afa3460a7a2a30
ram <ramachandra.choudki@gmail.com>
2018-06-26 02:39:54 +0530

    latest change

--- process_run.sh
+++ process_run.sh
@@ -4,5 +4,6 @@ if (( $(ps -ef | grep -v grep | grep -i $service | wc -l) > 0 ))
 then
 echo "$service is running fine!!!"
 else
+echo "starting $service"
 /home/ram/hadoop/hadoop-2.7.4-ex1/sbin/start-dfs.sh
 fi
